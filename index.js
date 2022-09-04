e5944be48e0bd1867d79a8e2b196c711f0025337
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-09-04 19:27:58 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+module.exports = app;
