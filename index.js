e1924c980408831489dfb379ed6a96a9ff1e3af5
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-09-04 19:31:45 -0300

    Add ok endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/ok", (req, res) => {
+  return res.status(200).send({ message: "ok", time: new Date() });
+});
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
